a19352576da8d43ab708bf8e80610712873af37b
Luciano Revillod <lrevillod2022@alu.uct.cl>
2023-01-10 20:56:48 -0300

    Searchbar and Keyframe

--- scripts/Searchbar.js
+++ scripts/Searchbar.js
@@ -1,5 +1,6 @@
 const SearchDiv = document.getElementById("header-searchbar-div");
 const HeadderSearchButton = document.getElementById("header-search-icon");
+const SearchInput = document.getElementById("header-searchbar-input");
 
 
 /* cambiar display  y activar animacion @keyframe al apretar HeaderSearchButton*/
@@ -10,13 +11,17 @@ HeadderSearchButton.addEventListener("click", (e) => {
         SearchDiv.style.display = "block";
 
         SearchDiv.style.animation = "header-searchbar-div-block 0.5s ease-in-out";
-
+    }
+    else {
+        SearchDiv.style.display = "none";
+        SearchDiv.style.animation = "header-searchbar-div-none 0.5s ease-in-out";
     }
 });
 
+
 document.addEventListener("click", (e) => {
 
-    if (e.target !== SearchDiv && e.target !== HeadderSearchButton) {
+    if (e.target !== SearchDiv && e.target !== HeadderSearchButton && e.target !== SearchInput) {
 
         SearchDiv.style.animation = "header-searchbar-div-none 0.5s ease-in-out";
         SearchDiv.style.display = "none";
