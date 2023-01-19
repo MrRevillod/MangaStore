48be2c43d077fcc6218339803abcbebec7364720
Luciano Revillod <lrevillod2022@alu.uct.cl>
2023-01-18 21:31:47 -0300

    update

--- scripts/switcher.js
+++ scripts/switcher.js
@@ -6,11 +6,6 @@ for (let i = 0; i < switcher.length; i++) {
     switcher[i].addEventListener("click", (event) => {
 
         tag.classList.toggle("dark-theme");
-
-        if (tag.classList.contains("dark-theme")) {
-            document.getElementById("catalogo").style.color = "#F5F5F5";
-
-        } else { document.getElementById("catalogo").style.color = "#050505" }
     });
 }
 
