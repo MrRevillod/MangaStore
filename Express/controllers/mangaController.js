a50d7738d306501c22216440d05d1787c1d30824
MrRevillod <lrevillod2022@alu.uct.cl>
2023-01-30 12:22:10 -0300

    ES6

--- Express/controllers/mangaController.js
+++ Express/controllers/mangaController.js
@@ -6,7 +6,8 @@ export const getMangas = async (req, res) => {
         const result = await Pool.query("SELECT * FROM mangas");
         const Mangas = result[0];
 
-        if (Mangas.length > 0) { res.status(200).json(Mangas); }
+        Mangas.length > 0 ? res.status(200).json(Mangas) : res.json({ message: "No Mangas found" });
 
     } catch (error) { res.status(500).json({ message: error.message }); }
 }
+
