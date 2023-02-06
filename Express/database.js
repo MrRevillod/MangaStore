b10d82217f32042f1a0e2ebde61781c95ffe9370
MrRevillod <lrevillod2022@alu.uct.cl>
2023-02-05 21:56:41 -0300

    Express API out

--- Express/database.js
+++
@@ -1,9 +0,0 @@
-import { createPool } from 'mysql2/promise';
-
-export const Pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '28081911',
-    port: 3306,
-    database: 'MangaStore'
-});
--- Express/routes/apiRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getMangas, getMangasWhere } from '../controllers/mangaController.js';
-
-const router = Router();
-router.get('/mangas', getMangas);
-router.get('/mangas/:phrase', getMangasWhere);
-
-export { router as apiRoutes }
