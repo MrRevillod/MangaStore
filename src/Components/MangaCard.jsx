f9892e48ac5ebba98eec3d1d2dc9e499074cbc0b
MrRevillod <lrevillod2022@alu.uct.cl>
2023-02-07 16:02:08 -0300

    better css

--- src/Components/MangaCard.jsx
+++ src/Components/MangaCard.jsx
@@ -9,26 +9,26 @@ export const MangaCard = (props) => {
 
     const FormatPrice = `$${props.Precio}`
 
-    const modalTarget = (e) => { if (e.target.className !== 'Cart-Button') setShowInfo(false) }
+    const modalTarget = (e) => { if (e.target.className !== 'bottom__details-btn') setShowInfo(false) }
 
     useEffect(() => { document.addEventListener('click', modalTarget) }, [])
 
     return (
         <div className="manga-card">
-            <div className="img-box" style={{ backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.6)), url(${props.Background})` }}>
+            <div className="manga-card__img-box" style={{ backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.6)), url(${props.Background})` }}>
 
-                <img className={`manga-card__image ${showInfo ? 'blur' : 'noblur'}`} src={props.Imagen} />
+                <img className={`manga-card__image ${showInfo ? 'gray' : 'nogray'}`} src={props.Imagen} />
 
-                <div className={`Modal-Info ${showInfo ? 'show' : 'hide'}`}>
+                <div className={`manga-card__info ${showInfo ? 'show' : 'hide'}`}>
                     <h4>{props.Titulo}</h4>
                     <h5>{FormatPrice}</h5>
                 </div>
             </div>
 
             <div className="manga-card__bottom">
-                <button className="Cart-Button" onClick={() => setShowInfo(!showInfo)}>View details</button>
-                <button className="Wish-Button" onClick={() => setIsLiked(!isLiked)}>
-                    <img className="Wish-Heart" alt="" width="28px"
+                <button className="bottom__details-btn" onClick={() => setShowInfo(!showInfo)}>View details</button>
+                <button className="bottom__wish-btn" onClick={() => setIsLiked(!isLiked)}>
+                    <img className="wish__btn-img" alt="" width="28px"
                         src={isLiked ? '/src/assets/icons/ico-liked.png' : '/src/assets/icons/ico-like.png'}
                     />
                 </button>
